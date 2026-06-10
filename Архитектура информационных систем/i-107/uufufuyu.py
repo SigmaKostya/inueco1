911042f803a6c43680da69c6c6ef92fe0d43fc86
SigmaKostya <konstantinsangin482@gmail.com>
2026-06-10 13:27:57 +0500

    Syangin

---
+++ Архитектура информационных систем/i-107/uufufuyu.py
@@ -0,0 +1,31 @@
+# 1 Привет, Мир
+print('Hello, World!')
+# 2 Комментарии
+# You know nothing, Jon Snow!
+# 3 Инструкции (Statements)
+print('Robert')
+print('Stannis')
+print('Renly')
+# 4 Как мы проверяем ваши решения
+print(10)
+# 5 Синтаксические ошибки
+print('What Is Dead May Never Die')
+# 6 Арифметические операции
+print(81 / 9)
+#Операторы
+print(6-(-81))
+#Коммутативная операция
+print(3**5)
+print((-8)/(-4))
+#Композиция операций
+print(8/2+5-(-3)/2)
+#Приоритет
+print(70*(3+4)/(8+2))
+#Числа с плавающей точкой
+print(0.39*0.22)
+#Линтер
+print( (5 **2)-(3* 7))
+#Кавычки
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+#Экранированные последовательности
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
